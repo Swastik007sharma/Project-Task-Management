b25dc5c321117b78868d0af14a742f1e52cf83e4
SWASTIK SHARMA <111640999+Swastik007sharma@users.noreply.github.com>
2026-02-05 00:07:52 +0530

    feat: add models for Project, Task, and User

---
+++ backend/models/project.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Project = mongoose.model("Project", projectSchema);
+module.exports = Project;
